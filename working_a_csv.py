f3b16457383631ed7c19ec0480676fb4c879da38
Ramon Barbosa <ramonbarbosafreitas@gmail.com>
2023-12-16 17:16:59 -0300

    Update working_a_csv.py

--- working_a_csv.py
+++ working_a_csv.py
@@ -2,15 +2,26 @@ import apache_beam as beam
 from apache_beam.io import ReadFromText
 from apache_beam.io import WriteToText
 from apache_beam.options.pipeline_options import PipelineOptions
+import re
 
 pipeline_options = PipelineOptions(argv=None)
 pipeline = beam.Pipeline(options=pipeline_options)
 
 
-input_file = 'purchase.csv'  # Replace with the actual path to your input file
-output_file = 'output.json'  # Replace with the desired path for the output file
+input_file = '/purchase.csv'  # Replace with the actual path to your input file
+output_file = '/output.json'  # Replace with the desired path for the output file
+
 
 cols = ["data", "id", "name", "value"]
+
+
+def valor_por_id(item):
+    ide, dic = item
+    for i in dic:
+        if bool(re.search(r'\d', i['value'])):
+            yield ("%s-%s" %(ide, i['year_month']), float(i['value']))
+        else:
+            yield ("%s-%s" %(ide, i['year_month']), 0.0)
 
 pcol_fluxo = (
     pipeline
@@ -21,8 +32,11 @@ pcol_fluxo = (
     | "obtem ano-mes do campo data" >> beam.Map(lambda x: {**x, 'year_month': "-".join(x['data'].split("-")[:2])})
     | "pega o id e cria uma chave pro json" >> beam.Map(lambda x: (x['id'], x)) 
     | "Agrupar por uid" >> beam.GroupByKey()
+    | "yield in function need use flatmap instead map" >> beam.FlatMap(valor_por_id)
+    | "somar valores" >> beam.CombinePerKey(sum) 
     | "printar" >> beam.Map(print)
-    #| 'WriteToText' >> beam.io.WriteToText(output_file)
+    #| 'WriteToText' >> WriteToText(output_file)
 )
+
 
 pipeline.run()
