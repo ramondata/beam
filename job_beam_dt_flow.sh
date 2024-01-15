cf1bff151226d809ab255ea2df829e4258b514ad
Ramon Barbosa <ramonbarbosafreitas@gmail.com>
2024-01-15 17:14:46 -0300

    Update job_beam_dt_flow.sh

--- job_beam_dt_flow.sh
+++ job_beam_dt_flow.sh
@@ -4,6 +4,8 @@ EXPORT BUCKET=gs:<bucket-name>
 
 gsutil mb -c STANDARD -l southamerica-east1 $BUCKET
 
+
+##Caso tenha subnet default:
 python -m apache_beam.examples.wordcount --project $DEVSHELL_PROJECT_ID \
   --runner DataflowRunner \
   --staging_location $BUCKET/staging \
@@ -11,3 +13,7 @@ python -m apache_beam.examples.wordcount --project $DEVSHELL_PROJECT_ID \
   --template_location $BUCKET/templates/<name-for-your-template-file> \
   --output $BUCKET/results/output \
   --region <region>
+
+##caso não tenha uma vpn default, necessita informar a url da subnet:
+python -m from_bq_to_bucket \ 
+  --subnet  https://www.googleapis.com/compute/v1/projects/cerc2-gestop-stg/regions/southamerica-east1/subnetworks/subnet-gestops-stg-02
